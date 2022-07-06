56d29c7849ff28d24b70f67df0fcb54b6964a076
Vihanga Pramudith <88036867+VihangaPramudith@users.noreply.github.com>
2022-07-06 12:26:36 +0530

    create mongodb connection and custom error handler

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const tasks = require('./routes/tasks');
+//const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
-const notFound = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+//const notFound = require('./middleware/not-found');
+//const errorHandlerMiddleware = require('./middleware/error-handler');
 
 //middleware
 
@@ -12,10 +12,10 @@ app.use(express.static('./public'));
 app.use(express.json());
 
 //routes
-app.use('/api/v1/tasks', tasks);
+// app.use('/api/v1/tasks', tasks);
 
-app.use(notFound);
-app.use(errorHandlerMiddleware);
+// app.use(notFound);
+// app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
