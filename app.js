2040958d89a68481e9802470d9dce5b7b5d0195d
Vihanga Pramudith <88036867+VihangaPramudith@users.noreply.github.com>
2022-07-06 16:32:33 +0530

    test the server

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-//const tasks = require('./routes/tasks');
-const connectDB = require('./db/connect');
-require('dotenv').config();
-//const notFound = require('./middleware/not-found');
-//const errorHandlerMiddleware = require('./middleware/error-handler');
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 //middleware
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 app.use(express.json());
 
 //routes
-// app.use('/api/v1/tasks', tasks);
+app.use("/api/v1/tasks", tasks);
 
-// app.use(notFound);
-// app.use(errorHandlerMiddleware);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI);
-        app.listen(PORT, () => {
-            console.log(`Server started on ${PORT}`);
-        });
-        
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`Server started on ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 start();
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 const {
-    getAllTasks,
-    createTask,
-    getTask,
-    updateTask,
-    deleteTask
-} = require('../controllers/tasks')
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/tasks");
 
-router.route('/').get(getAllTasks).post(createTask)
-router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
-export default router;
+module.exports = router;
